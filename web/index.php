e228b4a148775509573308fa2be6f349cc27d2f8
Colin DeCarlo <colin@thedecarlos.ca>
2013-03-10 09:01:43 -0400

    Register the security provider

--- web/index.php
+++ web/index.php
@@ -5,6 +5,22 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 
+// register the security and dependant providers
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+$app->register(new Silex\Provider\SessionServiceProvider());
+$app->register(new Silex\Provider\SecurityServiceProvider(), [
+	'security.firewalls' => [
+		'secured' => [
+			'pattern' => '^/admin',
+			'form' => ['login_path' => '/login', 'check_path' => '/admin/login_check'],
+			'logout' => ['logout_path' => '/admin/logout'],
+			'users' => [
+				'admin' => ['ROLE_ADMIN', '5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg==']
+			],
+		],
+	],
+]);
+
 // register the twig service provider
 $app->register(new Silex\Provider\TwigServiceProvider(),
 	['twig.path' => __DIR__ . '/../views']
