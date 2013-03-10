30b507e050482c880fb75b7036a9f22b71ccf307
Colin DeCarlo <colin@thedecarlos.ca>
2013-03-10 09:03:12 -0400

    add login route and view

--- web/index.php
+++ web/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
@@ -28,6 +30,13 @@ $app->register(new Silex\Provider\TwigServiceProvider(),
 
 $app->get('/', function () use ($app) {
 	return $app['twig']->render('home.twig');
+});
+
+$app->get('/login', function (Request $request) use ($app) {
+	return $app['twig']->render('login.twig', [
+		'error' => $app['security.last_error']($request),
+		'last_username' => $app['session']->get('_security.last_username'),
+	]);
 });
 
 $app->run();
